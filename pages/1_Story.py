90e5455acc05c9042b2880487e537d56db938e47
CristianG-png <155701238+CristianG-png@users.noreply.github.com>
2026-03-03 16:20:35 -0600

    Create 1_Story.py

---
+++ pages/1_Story.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+from charts import chart_q1
+
+# load your data
+df = pd.read_csv("data/yourfile.csv")
+team_summary = df.groupby(["Season", "Team"], as_index=False).agg({
+    "Points": "sum",
+    "GoalsFor": "sum",
+    "GoalsAgainst": "sum",
+    "GD": "sum"
+})
+
+# call the chart function
+st.altair_chart(chart_q1(team_summary), use_container_width=True)
