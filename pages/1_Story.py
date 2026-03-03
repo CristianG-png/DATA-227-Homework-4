91e211373a51717720b5de5280880096f5f52715
CristianG-png <155701238+CristianG-png@users.noreply.github.com>
2026-03-03 16:38:03 -0600

    Update 1_Story.py

--- pages/1_Story.py
+++ pages/1_Story.py
@@ -1,14 +1,10 @@
 import streamlit as st
-import pandas as pd
-from charts import chart_q1
+from data_loader import load_data
+from charts.chart import chart_q1, chart_q2, chart_q4_scatter, chart_q4_table
 
-# load your data
-df = pd.read_csv("data/yourfile.csv")
-team_summary = df.groupby(["Season", "Team"], as_index=False).agg({
-    "Points": "sum",
-    "GoalsFor": "sum",
-    "GoalsAgainst": "sum",
-    "GD": "sum"
-})
+df, team_summary, melted = load_data()
 
-st.altair_chart(chart_q1(team_summary), use_container_width=True)
+st.altair_chart(chart_q1(team_summary))
+st.altair_chart(chart_q2(melted))
+st.altair_chart(chart_q4_scatter(df))
+st.altair_chart(chart_q4_table(df))
