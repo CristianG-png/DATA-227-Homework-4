d42e08c50e3c38b3fce36e1b94db3d04017ce77d
CristianG-png <155701238+CristianG-png@users.noreply.github.com>
2026-03-03 22:49:18 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from data_loader import load_data
-from charts.chart import chart_q1, chart_q2, chart_q4_scatter, chart_q4_table
+from charts.chart import chart_q1, chart_q2_single_season, chart_q4_scatter, chart_q4_table
 from PIL import Image
 
 st.set_page_config(page_title="Analysis of Weather Patterns in Seattle:", layout="wide")
